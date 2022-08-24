10a7b6d369a27be1e364a70ddee1803249887e6e
Matthias Brumant <matthias.brumant@gmail.com>
2022-08-24 16:03:49 +0200

    Etape 2 entierement completée

--- app/glados/constants.py
+++ app/glados/constants.py
@@ -13,3 +13,14 @@ class EntityStatus(enum.IntEnum):
     on = 1
     off = 2
     unavailable = 3
+
+
+class EntityName(enum.IntEnum):
+    Thermometer = 1
+    Kitchen_light_1 = 2
+    living_room_light_1 = 3
+    living_room_light_2 = 4
+    television = 5
+    bedroom_switch_1 = 6
+    bedroom_light_1 = 7
+    Air_conditioner = 8
