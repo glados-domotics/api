9ed239a0d822b343846528b516962d3714ad9002
Horeb Parraud <44978959+HorebParraud@users.noreply.github.com>
2022-11-28 14:17:58 +0100

    🚧 entities CRUD - add put and get by id + test

--- app/glados/models/entity.py
+++ app/glados/models/entity.py
@@ -11,17 +11,24 @@ from glados.models.abc import BaseModel
 class Entity(db.Model, BaseModel):
     __tablename__ = "entities"
     __table_args__ = (
-        Index("entities_room_id_idx", "room_id"),)
+        Index("entities_room_id_idx", "room_id"),
+    )
 
     to_json_filter = ["room"]
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=lambda: uuid.uuid4().hex)
+
     name = db.Column(db.String(600), nullable=False)
     type = db.Column(db.Unicode(50), nullable=False)
     status = db.Column(db.Unicode(50), nullable=False)
     value = db.Column(db.String(600), nullable=True)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     room_id = db.Column(UUID(as_uuid=True), db.ForeignKey("rooms.id"), nullable=True)
+
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=True, default=datetime.now, onupdate=datetime.now)
 
     # Relationships
     room = db.relationship("Room", foreign_keys=[room_id], back_populates="entities")
+
+    class Meta:
+        ordering = ["room.name", "name"]
--- app/glados/repositories/entities.py
+++ app/glados/repositories/entities.py
@@ -1,11 +1,40 @@
-from glados.models import Entity
+import uuid
+
+from glados.models import Entity, Room
 
 
 def get_entities(filters):
     query = Entity.query
 
-    type = filters.get("type")
-    if type:
+    if type := filters.get("type"):
         query = query.filter(Entity.type == type)
 
+    if room_id := filters.get("room_id"):
+        query = query.filter(Entity.room_id == room_id)
+
+    if status := filters.get("status"):
+        query = query.filter(Entity.status == status)
+
     return query
+
+
+def get_entity(entity_id) -> Entity:
+    return Entity.query.filter_by(id=uuid.UUID(entity_id).hex).first()
+
+
+def update_entity(entity_id, data) -> Entity:
+    entity: Entity = get_entity(entity_id)
+
+    if name := data.get("name"):
+        entity.name = name
+
+    if type := data.get("type"):
+        entity.type = type
+
+    if room_id := data.get("room_id"):
+        room = Room.query.filter(Room.id == room_id).first()
+        entity.room = room
+
+    entity.save()
+
+    return entity
