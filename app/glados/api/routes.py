a213a6d4bddddd4a983038d83659aa5a6763968e
Horeb Parraud <44978959+HorebParraud@users.noreply.github.com>
2022-11-28 14:19:45 +0100

    updates routes

--- app/glados/api/routes.py
+++ app/glados/api/routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
 from flask_restful import Api
 
+from glados.api.entity.resources import EntitiesViews, EntityViews, EntitiesFilterViews
 from glados.api.misc import resources as misc_resources
-from glados.api.entity import resources as entity_resources
+from glados.api.room.resources import RoomsViews
 
 blueprint = Blueprint("api", __name__)
 api = Api(blueprint)
@@ -11,4 +12,11 @@ api = Api(blueprint)
 api.add_resource(misc_resources.VersionAPI, "/")
 
 # Entities endpoints
-api.add_resource(entity_resources.EntitiesAPI, "/entities")
+api.add_resource(EntitiesViews, "/entities")
+api.add_resource(EntityViews, "/<string:entity_id>/entities")
+
+# Filter endpoints
+api.add_resource(EntitiesFilterViews, "/filters")
+
+# Rooms endpoints
+api.add_resource(RoomsViews, "/rooms")
