3737555f9aee9c4fc5dd69f749ec20ec0e4b0506
Ben <ben.richebois@gmail.com>
2023-01-10 10:21:33 +0700

    Endpoint: add a new "status' filter + add a new "status" filter based on GET param

--- app/glados/repositories/entities.py
+++ app/glados/repositories/entities.py
@@ -5,7 +5,10 @@ def get_entities(filters):
     query = Entity.query
 
     type = filters.get("type")
+    status = filters.get("status")
     if type:
         query = query.filter(Entity.type == type)
+    if status:
+        query = query.filter(Entity.status == status)
 
     return query
